3f990888e5d2eafa0b98cc780041a8676bb445f1
Kristin Barr <kristinbarr@gmail.com>
2019-09-05 01:26:06 -0400

    added method for Lambda and MVP Lambdas

--- src/animalkingdom/Main.java
+++ src/animalkingdom/Main.java
@@ -3,7 +3,25 @@ package animalkingdom;
 import java.util.*;
 
 public class Main {
+
+  // create a method takes a lambda(in separate file). it will filter w 2 args.
+  // call it down below.
+  // static bc it's used within a class, void bc it's not returning anything
+  public static void printAnimals(ArrayList<AbstractAnimal> animals, CheckAnimal tester) {
+    for (AbstractAnimal a : animals) {
+      if (tester.test(a)) {
+        System.out.println(
+            "Name: " + a.getName() +
+            ", named in: " + a.getYear() +
+            ", movement: " + a.move() +
+            ", breaths with: " + a.breath() +
+            ", reproduces by: " + a.reproduce());
+      }
+    }
+  }
+
   public static void main(String[] args) {
+
     // mammals
     Mammal panda = new Mammal("Panda", 1869);
     Mammal zebra = new Mammal("Zebra", 1778);
@@ -11,7 +29,7 @@ public class Main {
     Mammal sloth = new Mammal("Sloth", 1804);
     Mammal armadillo = new Mammal("Armadillo", 1758);
     Mammal raccoon = new Mammal("Raccoon", 1758);
-    Mammal bigfoot = new Mammal("Bigfoor", 2021);
+    Mammal bigfoot = new Mammal("Bigfoot", 2021);
     // Birds
     Bird pigeon = new Bird("Pigeon", 1837);
     Bird peacock = new Bird("Peacock", 1821);
@@ -39,7 +57,60 @@ public class Main {
     System.out.println(catfish);
     System.out.println(perch);
 
-    // make an arraylist to add
-    // arrayLists can only have one data type
+    // an ArrayList must have one data type and all animals share AbstractAnimal
+    ArrayList<AbstractAnimal> animalList = new ArrayList<AbstractAnimal>();
+    animalList.add(panda);
+    animalList.add(zebra);
+    animalList.add(koala);
+    animalList.add(sloth);
+    animalList.add(armadillo);
+    animalList.add(raccoon);
+    animalList.add(bigfoot);
+    animalList.add(pigeon);
+    animalList.add(peacock);
+    animalList.add(toucan);
+    animalList.add(parrot);
+    animalList.add(swan);
+    animalList.add(salmon);
+    animalList.add(catfish);
+    animalList.add(perch);
+    System.out.println();
+
+    System.out.println("Animal List");
+    animalList.forEach((a) -> System.out.println(a));
+    System.out.println();
+
+    System.out.println("___ All Animals by descending year named ___");
+    animalList.sort((a1, a2) -> a1.getYear() - a2.getYear());
+    animalList.forEach((a) -> System.out.println(a));
+    System.out.println();
+
+    System.out.println("___ All Animals alphabetically ___");
+    animalList.sort((a1, a2) -> a1.getName().compareToIgnoreCase(a2.getName()));
+    animalList.forEach((a) -> System.out.println(a));
+    System.out.println();
+
+    System.out.println("___ All Animals by how they move ___");
+    animalList.sort((a1, a2) -> a1.move().compareToIgnoreCase(a2.move()));
+    animalList.forEach((a) -> System.out.println(a + " move: " + a.move()));
+    System.out.println();
+
+    System.out.println("___ Only Animals that breath with lungs ___");
+    printAnimals(animalList, (a -> a.breath().equals("lungs")));
+    System.out.println();
+
+    System.out.println("___ Only Animals that breath with lungs and named in 1758 ___");
+    printAnimals(animalList, (a -> a.getYear() == 1758));
+    System.out.println();
+
+    System.out.println("___ Only Animals that breath with lungs and lay eggs ___");
+    printAnimals(animalList, (a -> a.breath().equals("lungs") && a.reproduce().equals("eggs")));
+    System.out.println();
+
+    System.out.println("___ Only Animals alphabetically and named in 1758 ___");
+    animalList.sort((a1, a2) -> a1.getName().compareToIgnoreCase(a2.getName()));
+    printAnimals(animalList, (a -> a.getYear() == 1758));
+    System.out.println();
+
   }
 }
